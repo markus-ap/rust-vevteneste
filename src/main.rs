5e850b0594ce5e79145fb385593c46306be48b00
Markus AP <89122836+markus-ap@users.noreply.github.com>
2022-09-14 13:24:15 +0200

    hentar ut alle html-filar

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::{
     fs,
+    io,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
-    collections::HashMap
+    collections::HashMap,
+    ffi::OsStr, hash::Hash
 };
+
+use regex::Regex;
 
 fn main() {
     let lytter = TcpListener::bind("127.0.0.1:7878").unwrap();    
@@ -13,39 +17,33 @@ fn main() {
         let straum = straum.unwrap();
 
         thread::spawn(|| {
-            handter_kobling(straum);
+            let html = hent_html().unwrap();
+            handter_kobling(straum, &html);
         });
     }
 }
 
-fn handter_kobling(mut straum: TcpStream){
+fn handter_kobling(mut straum: TcpStream, sidar: &HashMap<String, String>){
     let buf_leser = BufReader::new(&mut straum);
     let spørrelinje = buf_leser.lines().next().unwrap().unwrap();
-    if spørrelinje.contains("GET /"){ handter_henting(&spørrelinje, straum); }
+    if spørrelinje.contains("GET /"){ handter_henting(&spørrelinje, straum, &sidar); }
 }
 
-fn handter_henting(spørrelinje: &String, mut straum: TcpStream){
-    use regex::Regex;
-
-    let sidar = HashMap::from([
-        ("", "indeks.html"),
-        ("hei", "hei.html"),
-    ]);
-
+fn handter_henting(spørrelinje: &String, mut straum: TcpStream, sidar: &HashMap<String, String>){
     let mønster = Regex::new(r"GET /(\w*) HTTP/1.1").unwrap();
     let fanget = mønster.captures(&spørrelinje).unwrap();
     let fanget = &fanget[1];
     
     let (status, filnamn) = if !sidar.contains_key(fanget){
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND".to_string(), "404.html".to_string())
     }
     else{
-        let side = sidar[fanget];
-        ("HTTP/1.1 200 OK", side)
+        let side = sidar[fanget].clone();
+        ("HTTP/1.1 200 OK".to_string(), side)
     };
 
     let innhald = fs::read_to_string(filnamn).unwrap();
-    let svar = lag_svar(status, innhald);
+    let svar = lag_svar(&status, innhald);
     
     straum.write_all(svar.as_bytes()).unwrap();
 }
@@ -53,4 +51,26 @@ fn handter_henting(spørrelinje: &String, mut straum: TcpStream){
 fn lag_svar(status: &str, innhald: String) -> String{
     let lengde = innhald.len();
     format!("{status}\r\nContent-Length: {lengde}\r\n\r\n{innhald}")
+}
+
+fn hent_html() -> io::Result<HashMap<String, String>>{
+    let mut resultat = HashMap::new();
+    
+    let mønster = Regex::new(r"./(\w+).html").unwrap();
+
+    for sti in fs::read_dir("./")?{
+        let sti = sti?.path();
+        if let Some("html") = sti.extension().and_then(OsStr::to_str){
+            let sti = sti.to_owned().into_os_string().into_string().unwrap();
+            if sti.contains(&"indeks"){
+                resultat.insert("".to_owned(), "indeks.html".to_owned());
+                resultat.insert("indeks".to_owned(), "indeks.html".to_owned());
+                continue;
+            }
+            let fanget = mønster.captures(&sti).unwrap();
+            let fanget = &fanget[1];
+            resultat.insert(fanget.to_string(), format!("{fanget}.html"));            
+        }
+    }
+    Ok(resultat)
 }
